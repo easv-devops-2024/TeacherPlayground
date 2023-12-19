74a2f7e18eed0d454bf76839bbfa2c4cc7c3a15d
Thomas Boulund Meng Hansen <thomasboulundmenghansen@Thomass-MacBook-Pro-6.local>
2023-12-19 15:29:26 +0100

    Added coverage

---
+++ DemoApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(GetGreetingMessage());
+    }
+
+    public static string GetGreetingMessage()
+    {
+        bool isDroneCI = Environment.GetEnvironmentVariable("DRONE") == "true";
+
+        if (isDroneCI)
+        {
+            return "Hello, Drone CI!";
+        }
+        else
+        {
+            return "Hello, World!";
+        }
+    }
+}
