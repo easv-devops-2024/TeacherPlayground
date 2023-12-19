35f611bc14967c58a2640b34b2b433cee5301fc5
Thomas Boulund Meng Hansen <thomasboulundmenghansen@Thomass-MacBook-Pro-6.local>
2023-12-19 15:28:33 +0100

    Added test project

---
+++ UnitTests/Tests.cs
@@ -0,0 +1,30 @@
+using Xunit;
+
+public class ProgramTests
+{
+    [Fact]
+    public void GetGreetingMessage_ShouldReturnDroneCI_GivenDroneCIBranch()
+    {
+        // Arrange
+        Environment.SetEnvironmentVariable("DRONE", "true");
+
+        // Act
+        var result = Program.GetGreetingMessage();
+
+        // Assert
+        Assert.Equal("Hello, Drone CI!", result);
+    }
+
+    [Fact]
+    public void GetGreetingMessage_ShouldReturnHelloWorld_GivenNonDroneCIBranch()
+    {
+        // Arrange
+        Environment.SetEnvironmentVariable("DRONE", "false");
+
+        // Act
+        var result = Program.GetGreetingMessage();
+
+        // Assert
+        Assert.Equal("Hello, World!", result);
+    }
+}
